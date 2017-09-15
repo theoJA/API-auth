bc1321e3aad88085b03d4c38753c19b9d6ab6a14
theoJA <theophilus.ja@gmail.com>
2017-09-14 22:25:02 -0400

    used passport.authenticate for signIn and secret

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = require('express-promise-router')(); // () at the end means we're invoking the function straight away
+const passport = require('passport');
+const passportConf = require('../passport')
 
 const { validateBody, schemas } = require('../helpers/routeHelpers')
 const UsersController = require('../controllers/users')
 
+const passportSignIn = passport.authenticate('local', { session: false });
+const passportJWT = passport.authenticate('jwt', { session: false });
+
 router.route('/signup')
-  .post(validateBody(schemas.authSchema), UsersController.signUp);
+  .post(validateBody(schemas.authSchema), 
+        UsersController.signUp);
 
 router.route('/signin')
-  .post(UsersController.signIn);
+  .post(validateBody(schemas.authSchema), 
+        passportSignIn, 
+        UsersController.signIn);
 
 router.route('/secret')
-  .get(UsersController.secret);
+  .get(passportJWT, 
+       UsersController.secret);
 
 module.exports = router;
