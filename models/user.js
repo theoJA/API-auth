20f5e587b529727f07e39f2848a4ed1a258aeb87
theoJA <theophilus.ja@gmail.com>
2017-09-14 22:22:17 -0400

    Hashing and comparing passwords

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 // Create a schema
@@ -14,6 +15,33 @@ const userSchema = new Schema({
     required: true
   }
 });
+
+// This function allows us to execute code before a method is performed on the
+//  instance of User. This would be perfect for hashing the password before 
+//  saving a new user into the DB. 'save' is the method being called on the 
+//  instance of User after this function is ran. 
+// Also using function instead of arrow function allows us to use this.password 
+userSchema.pre('save', async function(next) {
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Generate a password hash (salt + hash)
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    // Re-assign hashed version over original, plain text password
+    this.password = passwordHash;
+    next();
+  } catch(error) {
+    next(error);
+  }
+});
+
+userSchema.methods.isValidPassword = async function(newPassword) {
+  try {
+    return await bcrypt.compare(newPassword, this.password);
+  } catch(error) {
+    throw new Error(error);
+  }
+}
 
 // Create a model
 const User = mongoose.model('user', userSchema);
