503435bbe9e180f59dcc1e4d9a61b60a7aaad01f
theoJA <theophilus.ja@gmail.com>
2017-09-14 22:38:44 -0400

    First commit. Added JWT and local strategy

---
+++ passport.js
@@ -0,0 +1,51 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
+const { JWT_SECRET } = require('./configuration')
+const User = require('./models/user')
+
+// JWT STRATEGY
+passport.use(new JwtStrategy({
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: JWT_SECRET
+}, async (payload, done) => {
+  try {
+    // Find the user specified in token
+    const user = await User.findById(payload.sub);
+    // If user doesn't exist, handle it
+    if (!user) {
+      return done(null, false);
+    }
+    // Otherwise return the user
+    done(null, user);
+    
+  } catch(error) {
+    done(error, false);
+  }
+}));
+
+// LOCAL STRATEGY
+passport.use(new LocalStrategy({
+  usernameField: 'email'
+}, async (email, password, done) => {
+  try {
+    // Find the user given the email
+    const user = await User.findOne({ email });
+    // If not, handle it
+    if (!user) {
+      return done(null, false);
+    }
+    // Check if the password is correct
+    const isMatch = await user.isValidPassword(password);
+    // If not, handle it
+    if (!isMatch) {
+      return done(null, false);
+    }
+    // Otherwise, return the user
+    done(null, user);
+
+  } catch(error) {
+    done(error, false);
+  }
+}));
